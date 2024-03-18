1ee02f186013b9fb08a5ca0ac978fbb8d7ec3a16
Jules Pelissou <jules.pelissou@gmail.com>
2024-03-18 15:25:08 +0100

    Vérification fichiers

--- Assets/TutorialInfo/Scripts/Readme.cs
+++ Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,18 +1,6 @@
 ﻿using System;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class Readme : ScriptableObject {
-	public Texture2D icon;
-	public string title;
-	public Section[] sections;
-	public bool loadedLayout;
-	
-	[Serializable]
-	public class Section {
-		public string heading, text, linkText, url;
-	}
-=======
 public class Readme : ScriptableObject
 {
     public Texture2D icon;
@@ -25,5 +13,4 @@ public class Readme : ScriptableObject
     {
         public string heading, text, linkText, url;
     }
->>>>>>> a3c3f785000291e3a6ec2b0d00a020a93b47616d
 }
